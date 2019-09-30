43bf33098687ee09ad83c31ca8165bf256d0e7ce
Mike Oleynik <oleynik.mike@yandex.ru>
2019-09-30 14:07:28 +0300

    Adding output a set number of maximum digits

--- lib/nmax/cli.rb
+++ lib/nmax/cli.rb
@@ -1,8 +1,25 @@
+# frozen_string_literal: true
+
 module Nmax
   class Cli
+    MIN_SIZE = 1
+    MAX_SIZE = 1000
+    REGEXP_FOR_FIND = /\d{#{MIN_SIZE},#{MAX_SIZE}}/.freeze
+
+    attr_reader :number, :text
+
     def initialize(text, number)
       @text = text
       @number = number
+    end
+
+    def calculate_number
+      numbers_of_digit = text.scan(REGEXP_FOR_FIND).map(&:to_i).uniq
+      if numbers_of_digit.empty?
+        'No digits found'
+      else
+        numbers_of_digit.max(number.to_i)
+      end
     end
   end
 end
--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -1,9 +1,23 @@
+# frozen_string_literal: true
+
 RSpec.describe Nmax do
-  it "has a version number" do
+  let(:number) { '5' }
+  let(:text) { 'Obama made $2.7 million in 2008' }
+  let(:cli) { Nmax::Cli.new(text, number) }
+
+  it 'has a version number' do
     expect(Nmax::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'number of digit is exist' do
+    expect(cli.calculate_number).not_to be_nil
+  end
+
+  it 'return sorted numbers' do
+    expect(cli.calculate_number).to eq([2008, 7, 2])
+  end
+
+  it 'number of digit <= passed value' do
+    cli.calculate_number.size <= number.to_i
   end
 end
